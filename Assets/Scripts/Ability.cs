c4f36126cfbfc4acfcdbcd08ad7ada9fbb8b4190
superflouz <superflouz@hotmail.com>
2020-03-17 21:22:11 +0100

    Add base script for characters

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct CharacterAbility
+{
+    public Ability ability;
+    public int manaCost;
+    public int cooldown;
+}
+
+public class Ability : MonoBehaviour
+{
+
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour, IBuyable
+{
+    public Sprite icon;
+    public Sprite Icon { get { return icon; } }
+
+    public int price;
+    public int Price { get { return price; } }
+
+    public int health;
+
+    public Action action;
+    public List<CharacterAbility> abilities;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
