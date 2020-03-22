2de604823bac8450432a5cfee73529ad6bdc3355
David Tara <davidtara@bluewin.ch>
2020-03-22 17:22:43 +0100

    Arrow Trajectory and Hit

--- Assets/Scripts/Attacks/ArrowAttack.cs
+++ Assets/Scripts/Attacks/ArrowAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ArrowAttack : Attack
 {
+    public Arrow arrow;
 
     // Start is called before the first frame update
     new void Start()
@@ -17,24 +18,11 @@ public class ArrowAttack : Attack
         base.Update();
     }
 
-    private Vector2 CalculateQuadraticBezierCurve(float t, Vector2 P0, Vector2 P1, Vector2 P2)
+    protected override void ExecuteAttack(Entity target)
     {
-        //P0 is origin
-        //P1 is the top angle of the base triangle
-        //P2 is target
-
-        // B(t) = ((1-t)^2 * P0) + (2(1-t) * t * P1) + (t^2 * P2)
-        //      =    (u ^2 * P0) +  (2 * u * t * P1) + (t^2 * P2)
-        //      =      (uu * P0) +  (2 * u * t * P1) +  (tt * P2)
-
-        float u = 1 - t;
-        float tt = t * t;
-        float uu = u * u;
-
-        Vector2 point = (uu * P0) + (2 * u * t * P1) + (tt * P2);
-
-        return point;
+        Arrow a; 
+        a = Instantiate(arrow, transform.position, Quaternion.identity);
+        a.Target = target;
     }
-
 
 }
