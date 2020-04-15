2d653d1c7f28ef11a5493ca548fb8ed8a43ecc68
David Tara <davidtara@bluewin.ch>
2020-04-15 15:57:19 +0200

    Shop UI and Functionnalities

--- Assets/Scripts/IBuyable.cs
+++ Assets/Scripts/IBuyable.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public interface IBuyable
 {
     Sprite Icon { get; }
+    string Name { get; }
     int Price { get; }
 }
---
+++ Assets/Scripts/UI/ButtonListButton.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonListButton : MonoBehaviour
+{
+    public ButtonListControl buttonControl;
+    public Image icon;
+    public Text text;
+
+    private Buyable buyable;
+    public Buyable Buyable
+    {
+        get { return buyable; }
+        set { buyable = value; }
+    }
+
+    private Entity entity;
+    public Entity Entity
+    {
+        get { return entity; }
+        set { entity = value; }
+    }
+
+    private int price;
+    public int Price
+    {
+        get { return price; }
+        set
+        {
+            text.text = CharaName + "\n\n" + value;
+            price = value;
+        }
+    }
+
+    private string charaName;
+    public string CharaName
+    {
+        get { return charaName; }
+        set
+        {
+            text.text = value + "\n\n" + CharaDescr;
+            charaName = value;
+        }
+    }
+
+    private string charaDescr;
+    public string CharaDescr
+    {
+        get { return charaDescr; }
+        set { charaDescr = value; }
+    }
+
+    private Sprite charaIcon;
+    public Sprite CharaIcon
+    {
+        get { return charaIcon; }
+        set
+        {
+            icon.sprite = value;
+            charaIcon = value;
+        }
+    }
+
+    public void OnClick()
+    {
+        // Buy Character on click
+        buttonControl.ButtonClicked(Entity, Buyable);
+    }
+}
