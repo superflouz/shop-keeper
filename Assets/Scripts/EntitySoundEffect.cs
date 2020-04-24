0dc474f45ad5af9559c54bc04159e2308ed3907d
Alain Hardy <alain.hardy@protonmail.com>
2020-04-24 17:47:56 +0200

    Create script for managing sound effects of entity

---
+++ Assets/Scripts/EntitySoundEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySoundEffect : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] attackSounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayAttackSound()
+    {
+        if (attackSounds.Length <= 0) return;
+        int index = Random.Range(0, attackSounds.Length);
+        audioSource.PlayOneShot(attackSounds[index]);
+    }
+}
