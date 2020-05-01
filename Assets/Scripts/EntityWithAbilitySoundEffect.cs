0ccbaa07be39c426b156ced71d24ebb4dfe91963
Alain Hardy <alain.hardy@protonmail.com>
2020-05-01 18:25:58 +0200

    Create script to play sounds for ability

---
+++ Assets/Scripts/EntityWithAbilitySoundEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityWithAbilitySoundEffect : EntitySoundEffect
+{
+    public AudioClip[] abilitySounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playAbilitySound() {
+        if (abilitySounds.Length <= 0) return;
+        int index = Random.Range(0, abilitySounds.Length);
+        audioSource.PlayOneShot(abilitySounds[index]);
+    }
+}
